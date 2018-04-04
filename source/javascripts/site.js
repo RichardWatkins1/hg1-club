e55cdbffffa9eb37e7c515290820469f0982b530
Richard Watkins <richardwatkins1@gmail.com>
2018-04-04 20:42:22 +0100

    added ajax form

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,9 @@
 // This is where it all goes :)
+$("#my-form").submit(function(e) {
+  e.preventDefault();
+
+  var $form = $(this);
+  $.post($form.attr("action"), $form.serialize()).then(function() {
+    alert("Thanks for subscribing"!);
+  });
+});
