8f3f2e33f4157bbac06d704e7e8e3863e1d81c48
Richard Watkins <richardwatkins1@gmail.com>
2018-04-04 20:45:00 +0100

    updated js

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,9 +1,9 @@
 // This is where it all goes :)
-$("#my-form").submit(function(e) {
-  e.preventDefault();
+$("#my-form").submit(function (e) {
+  e.preventDefault()
 
   var $form = $(this);
-  $.post($form.attr("action"), $form.serialize()).then(function() {
-    alert("Thanks for subscribing"!);
+  $.post($form.attr('action'), $form.serialize()).then(function () {
+    alert('Thanks for subscribing!')
   });
 });
